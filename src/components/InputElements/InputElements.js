c81c8aed58b0ec72e1b27f21ffaf8e419a0e61ee
Laura Bigoni <laurabigoni.92@gmail.com>
2022-07-13 15:06:26 +0200

    try to loop the cards for the value from range

--- src/components/InputElements/InputElements.js
+++ src/components/InputElements/InputElements.js
@@ -1,37 +1,9 @@
 import React from "react";
-import { Component } from "react";
 
-class InputElements extends Component {
-  constructor(props) {
-    super(props);
-    this.handleValue = this.handleValue.bind(this);
-    this.handleBackground = this.handleBackground.bind(this);
-    this.state = {
-      bgColor: "#FFFFFF",
-      value: 3,
-    };
-  }
-
-  handleValue(e) {
-    console.log(this);
-    this.setState({
-      value: e.target.value,
-    });
-    console.log(this.state.value);
-  }
-  handleBackground(e) {
-    this.setState({
-      bgColor: e.target.value,
-    });
-    console.log(this.state.bgColor);
-  }
-
-  render() {
-    const value = this.state.value;
-    const bgColor = this.state.bgColor;
-    return (
-      <div
-        className="
+function InputElements(props) {
+  return (
+    <div
+      className="
 			input-container
 			flex
 			justify-end
@@ -41,9 +13,9 @@ class InputElements extends Component {
 			md:flex-row
 			items-center
 		"
-      >
-        <div
-          className="
+    >
+      <div
+        className="
 				mix-blend-difference
 				text-gray-100
 				flex
@@ -52,38 +24,37 @@ class InputElements extends Component {
 				md:py-3
 				py-5
 			"
-        >
-          <label>Number of cards:</label>
-          <input
-            type="range"
-            min="0"
-            max="42"
-            step="1"
-            name="cards-range"
-            id="cards-range"
-            className="mx-3"
-            value={value}
-            onInput={this.handleValue}
-          />
+      >
+        <label>Number of cards:</label>
+        <input
+          type="range"
+          min="0"
+          max="42"
+          step="1"
+          name="cards-range"
+          id="cards-range"
+          className="mx-3"
+          value={props.value}
+          onChange={props.handleValueChange}
+        />
 
-          <output>{this.state.value}</output>
-        </div>
-
-        <div className="flex items-center basis-1/2 md:py-3">
-          <label className="pr-4 mix-blend-difference text-gray-100">
-            Background color:
-          </label>
-          <input
-            type="color"
-            name="color-picker"
-            id="color-picker"
-            value={bgColor}
-            onInput={this.handleBackground}
-          />
-        </div>
+        <output>{props.value}</output>
       </div>
-    );
-  }
+
+      <div className="flex items-center basis-1/2 md:py-3">
+        <label className="pr-4 mix-blend-difference text-gray-100">
+          Background color:
+        </label>
+        <input
+          type="color"
+          name="color-picker"
+          id="color-picker"
+          value={props.bgColor}
+          onInput={props.handleBgChange}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default InputElements;
--- src/components/SliderPage/SliderPage.js
+++ src/components/SliderPage/SliderPage.js
@@ -4,12 +4,29 @@ import SliderCard from "../SliderCard/SliderCard";
 import InputElements from "../InputElements/InputElements";
 import NavigationElements from "../NavigationElements/NavigationElements";
 
-function SliderPage() {
-  return (
-    <div className={styles.SliderPage}>
-      <div className="slider flex flex-col justify-center">
-        <div
-          className="
+class SliderPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleBgChange = this.handleBgChange.bind(this);
+    this.state = {
+      bgColor: "#F4EAE1",
+      value: 3,
+    };
+  }
+  handleValueChange(e) {
+    this.setState({ value: e.target.value });
+    console.log(this.state.value);
+  }
+  handleBgChange(e) {
+    this.setState({ bgColor: e.target.value });
+  }
+  render() {
+    return (
+      <div className={styles.SliderPage}>
+        <div className="slider flex flex-col justify-center">
+          <div
+            className="
             flex
             md:flex-row
             flex-col
@@ -17,9 +34,9 @@ function SliderPage() {
             items-center
             basis-basis-1/5
           "
-        >
-          <h1
-            className="
+          >
+            <h1
+              className="
               mix-blend-difference
               text-gray-100 text-4xl
               font-bold
@@ -27,19 +44,37 @@ function SliderPage() {
               text-center
               pt-2
             "
-          >
-            Section Cards
-          </h1>
-          <InputElements />
-        </div>
-        <div className="flex justify-center basis-3/5">
-          <div className="slider-cards flex flex-row items-center justify-start">
-            <SliderCard className="w-60 h-80 ease-in duration-300 card shrink-0" />
+            >
+              Section Cards
+            </h1>
+            <InputElements
+              value={this.state.value}
+              handleValueChange={this.handleValueChange}
+              bgColor={this.state.bgColor}
+              handleBgChange={this.handleBgChange}
+            />
           </div>
+          <div className="flex justify-center basis-3/5">
+            <div className="slider-cards flex flex-row items-center justify-start">
+              {(() => {
+                let cards = [];
+                for (let i in this.state.value) {
+                  cards.push(
+                    <SliderCard
+                      className="w-60 h-80 ease-in duration-300 card shrink-0"
+                      key={i}
+                    />
+                  );
+                }
+                console.log(cards);
+                return cards;
+              })()}
+            </div>
+          </div>
+          <NavigationElements />
         </div>
-        <NavigationElements />
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default SliderPage;
